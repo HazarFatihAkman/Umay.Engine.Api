d53809755d365cdea869734aab6cc98c1acbad84
HazarFatihAkman <akman.hazar.fatih@gmail.com>
2026-03-06 15:52:43 +0300

    asset manager part 1

---
+++ include/asset_manager.h
@@ -0,0 +1,17 @@
+#ifndef UMAY_ENGINE_API_ASSET_MANAGER_H
+#define UMAY_ENGINE_API_ASSET_MANAGER_H
+
+#include <stdint.h>
+
+#define FOLDER_PATH "assets/"
+
+typedef struct asset_route {
+  uint32_t flag;
+  char *folder;
+} asset_route_t;
+
+void init_asset_manager(asset_route_t asset_routes[]);
+char *find_asset_folder_by_flag(uint32_t flag);
+
+#endif // UMAY_ENGINE_API_ASSET_MANAGER_H
+
---
+++ src/asset_manager.c
@@ -0,0 +1,15 @@
+#include "../include/asset_manager.h"
+
+#include <stdint.h>
+#include <unistd.h>
+
+static const asset_route_t *asset_route;
+
+void init_asset_manager(asset_route_t asset_routes[]) {
+  asset_routes = asset_routes;
+}
+
+char *find_asset_folder_by_flag(uint32_t flag) {
+  return NULL;
+}
+
