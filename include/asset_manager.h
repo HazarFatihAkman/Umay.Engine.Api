cf2d603d37d681eb1ae8d118f6020bd37d4457aa
HazarFatihAkman <akman.hazar.fatih@gmail.com>
2026-03-15 23:04:57 +0300

    working on assets

--- include/asset_manager.h
+++ include/asset_manager.h
@@ -12,7 +12,7 @@ typedef struct asset_route {
 } asset_route_t;
 
 void init_asset_manager(asset_route_t asset_routes[]);
-char *find_asset_folder_by_flag(uint32_t flag);
+char *find_asset_folder_by_flag(uint32_t flag, const char *ext);
 
 #endif // UMAY_ENGINE_API_ASSET_MANAGER_H
 
